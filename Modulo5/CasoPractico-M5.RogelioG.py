aff7ad5ead991edbf6c5294d85d84a1580651777
Rogelio G <est94.rguzman@gmail.com>
2025-06-25 22:54:31 -0600

    Agrego CasoPractico-M5.RogelioG.py

---
+++ Modulo5/CasoPractico-M5.RogelioG.py
@@ -0,0 +1,7 @@
+# Programa para contar numeros pares del 0 al 100
+# Programa para posteriormente subirlo a Github
+# Rogelio G - Bootcamp Desarrollo WEB - UVM
+
+for i in range(1, 101):
+    if i % 2 == 0:
+        print(i, end=" ")
